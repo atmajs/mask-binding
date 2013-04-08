88993b4d659f529db7414175f49d2b61d2e0f819
Alexander Kit <source.kit@gmail.com>
2013-04-08 10:05:59 +0200

    [++] travis build

--- builds/mask.binding.js
+++ builds/mask.binding.js
@@ -4,24 +4,24 @@
 
 	"use strict";
 
-	// import /src/vars.js
+	// import ../src/vars.js
 
-	// import /src/util/object.js
-	// import /src/util/array.js
-	// import /src/util/dom.js
-	// import /src/util/compo.js
+	// import ../src/util/object.js
+	// import ../src/util/array.js
+	// import ../src/util/dom.js
+	// import ../src/util/compo.js
 
-	// import /src/bindingProvider.js
+	// import ../src/bindingProvider.js
 
-	// import /src/mask-handler/visible.js
-	// import /src/mask-handler/bind.js
-	// import /src/mask-handler/dualbind.js
-	// import /src/mask-handler/validate.js
-	// import /src/mask-handler/validate.group.js
+	// import ../src/mask-handler/visible.js
+	// import ../src/mask-handler/bind.js
+	// import ../src/mask-handler/dualbind.js
+	// import ../src/mask-handler/validate.js
+	// import ../src/mask-handler/validate.group.js
 
-	// import /src/mask-util/bind.js
+	// import ../src/mask-util/bind.js
 
-	// import /src/sys/sys.js
+	// import ../src/sys/sys.js
 
 
 }(typeof window === 'undefined' ? global : window, typeof mask === 'undefined' ? null : mask));
