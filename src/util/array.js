f567ff393a0bdeddd552e119593cba9fbf789e0c
Alexander Kit <source.kit@gmail.com>
2013-04-14 14:22:31 +0200

    [-] arr_toArray

--- src/util/array.js
+++ src/util/array.js
@@ -28,12 +28,6 @@ function arr_remove(array /*, .. */){
 }
 
 
-function arr_toArray(args, start) {
-	return __array_slice.call(args, start == null ? 0 : start);
-}
-
-
-
 function arr_addObserver(arr, callback) {
 
 	if (arr.__observers == null) {
@@ -43,36 +37,12 @@ function arr_addObserver(arr, callback) {
 		});
 	}
 
-	////////function wrap(method) {
-	////////	arr[method] = function() {
-	////////		var callbacks = this.__observers,
-	////////			args = arr_toArray(arguments),
-	////////			result = Array.prototype[method].apply(this, args);
-	////////
-	////////
-	////////		if (callbacks == null || callbacks.length === 0) {
-	////////			return result;
-	////////		}
-	////////
-	////////
-	////////		for(var i = 0, x, length = callbacks.length; i < length; i++){
-	////////			x = callbacks[i];
-	////////			if (typeof x === 'function') {
-	////////
-	////////				x(this, method, args);
-	////////			}
-	////////		}
-	////////
-	////////		return result;
-	////////	};
-	////////}
-
 	var i = 0,
 		fns = ['push', 'unshift', 'splice', 'pop', 'shift', 'reverse', 'sort'],
 		length = fns.length,
 		method;
+
 	for (; i < length; i++) {
-		//////wrap(fns[i]);
 		method = fns[i];
 		arr[method] = _array_methodWrapper.bind(arr, arr[method], method);
 	}
@@ -82,7 +52,7 @@ function arr_addObserver(arr, callback) {
 
 function _array_methodWrapper(original, method) {
 	var callbacks = this.__observers,
-		args = arr_toArray(arguments, 2),
+		args = __array_slice.call(arguments, 2),
 		result = original.apply(this, args);
 
 
