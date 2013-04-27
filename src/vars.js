c7dcccbc265c969d59601b6257d0c05a25d6076f
Alexander Kit <source.kit@gmail.com>
2013-04-27 16:24:32 +0200

    [-] remove domLib warning

--- src/vars.js
+++ src/vars.js
@@ -1,7 +1,3 @@
 var $ = window.jQuery || window.Zepto || window.$,
 
 	__array_slice = Array.prototype.slice;
-
-if ($ == null){
-	console.warn('Without jQuery/Zepto etc. binder is limited (mouse dom event bindings)');
-}
