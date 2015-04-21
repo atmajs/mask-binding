e7575598a1eb4e8da3a62c3f17b9756e550bccdf
Alex Kit <source.kit@gmail.com>
2015-04-21 14:38:49 +0200

    * (DateObject Binding) get-set UTC

--- src/DomObjectTransport.js
+++ src/DomObjectTransport.js
@@ -137,9 +137,9 @@ var DomObjectTransport;
 			objectWay: {
 				get: objectWay.get,
 				set: DateTimeDelegate.objSet(function(a, b){
-					a.setFullYear(b.getFullYear());
-					a.setMonth(b.getMonth());
-					a.setDate(b.getDate());
+					a.setUTCFullYear(b.getUTCFullYear());
+					a.setUTCMonth(b.getUTCMonth());
+					a.setUTCDate(b.getUTCDate());
 				})
 			}
 		},
