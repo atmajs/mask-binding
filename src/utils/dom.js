e37ba33f5c59f721bc53b63b1c1de59cd7d7ae42
Alex Kit <source.kit@gmail.com>
2016-04-27 11:58:06 +0200

    fix (remove element) do nothing if already

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,26 +1,30 @@
+var dom_removeElement,
+	dom_removeAll,
+	dom_insertAfter,
+	dom_insertBefore;
+(function(){
 
-function dom_removeElement(node) {
-	return node.parentNode.removeChild(node);
-}
+	dom_removeElement = function(node) {
+		var parent = node.parentNode;
+		if (parent == null)
+			return node;
 
-function dom_removeAll(array) {
-	if (array == null) 
-		return;
-	
-	var imax = array.length,
-		i = -1;
-	while ( ++i < imax ) {
-		dom_removeElement(array[i]);
-	}
-}
-
-function dom_insertAfter(element, anchor) {
-	return anchor.parentNode.insertBefore(element, anchor.nextSibling);
-}
-
-function dom_insertBefore(element, anchor) {
-	return anchor.parentNode.insertBefore(element, anchor);
-}
-
-
-
+		return parent.removeChild(node);
+	};
+	dom_removeAll = function(array) {
+		if (array == null) 
+			return;
+		
+		var imax = array.length,
+			i = -1;
+		while ( ++i < imax ) {
+			dom_removeElement(array[i]);
+		}
+	};
+	dom_insertAfter = function(element, anchor) {
+		return anchor.parentNode.insertBefore(element, anchor.nextSibling);
+	};
+	dom_insertBefore = function(element, anchor) {
+		return anchor.parentNode.insertBefore(element, anchor);
+	};
+}());
